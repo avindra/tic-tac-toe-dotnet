56049686fc3450b3d3475477d15291c9d2880ae1
Avindra Goolcharan <aavindraa@gmail.com>
2010-08-03 21:21:38 +0000

    This is heavy code optimization. Main optimization is using arrays to extremely reduce the amount of code. Not only will the program run faster and more efficiently now, but this code is much easier to read.
    -Increased maintainability index
    -Lowered cyclomatic complexity
    
    Further optimizations coming soon, wanted to commit this immediately though.

--- TicTacToe/btnSquare.cs
+++ TicTacToe/btnSquare.cs
@@ -6,24 +6,43 @@ using System.Windows.Forms;
 
 namespace TicTacToe
 {
-
-    public class btnSquare : Button
+	/// <summary>
+	/// A class which provides useful and common
+	/// functionality on top of the Button class for
+	/// Tic-Tac-Toe.
+	/// </summary>
+    class btnSquare : Button
     {
+		/// <summary>
+		/// A private instance variable for storing
+		/// whether or not the current square is an X.
+		/// 
+		/// Cannot be used publicly because it needs
+		/// to be used in conjunction with whether or not
+		/// the Button is enabled.
+		/// </summary>
         private bool blnIsX = false;
-        public bool isX()
+
+		/// <summary>
+		/// Returns whether or not the current square is an "X".
+		/// </summary>
+		public bool isX()
         {
             return !this.Enabled && blnIsX;
         }
 
+		/// <summary>
+		/// Returns whether or not the current square is an "O".
+		/// </summary>
         public bool isO()
         {
             return !this.Enabled && !blnIsX;
         }
 
-        public bool isFree()
-        {
-            return this.Enabled;
-        }
+		public bool autoCheck(bool checkX)
+		{
+			return checkX ? isX() : isO();
+		}
 
         public void setX()
         {
@@ -39,6 +58,10 @@ namespace TicTacToe
             this.Enabled = false;
         }
 
+		/// <summary>
+		/// Call this method whenever you need to
+		/// unset or reset the square.
+		/// </summary>
         public void unset()
         {
             this.Text = "";
